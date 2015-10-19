1c23ed938c0a057c1446d1356a1b1a9d0444c3be
freyeso01 <freyeso01@aula1-19>
2015-10-19 18:44:54 +0200

    Ejercicio para manual de git-hub

---
+++ ManualGrupo/src/manualgrupo/EjercicioFernando.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manualgrupo;
+
+/**
+ *
+ * @author freyeso01
+ */
+public class EjercicioFernando {
+    
+    
+    public static void main(String[] args) {
+        
+        int n1=70,n2=20,n3=10,suma;
+        
+        suma=n1+n2+n3;
+        
+        System.out.println("La suma de "+n1+", "+n2+" y "+n3+" da como resultado: "+suma);
+        
+    }
+    
+}
