1ae21970dccb67b00b2d540b81f9eb068ca98a25
rrgarciac01 <rrgarciac01@aula1-21>
2015-10-19 18:44:33 +0200

    Hecho un cambio

---
+++ ManualGrupo/src/manualgrupo/Raul.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manualgrupo;
+
+/**
+ *
+ * @author rrgarciac01
+ */
+public class Raul {
+    //
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i < 7; i++) {
+            System.out.print("Hola" + i + "-");
+        }
+
+    }
+}
