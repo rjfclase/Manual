cfa6cb2cdadf230a137de47e0ba9ad45c2a511be
jlprietof01 <jlprietof01@aula1-20>
2015-10-19 18:43:33 +0200

    EjercicioJose.

---
+++ ManualGrupo/src/manualgrupo/EjercicioJoséLuis.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manualgrupo;
+
+/**
+ *
+ * @author jlprietof01
+ */
+public class EjercicioJoséLuis {
+    
+    public static void main(String[] args){
+    int numero1=30,numero2=20;
+    int resta=numero1-numero2;
+        System.out.println("La resta de"+numero1+"y el"+numero2+" es: "+resta);  
+    }
+}
